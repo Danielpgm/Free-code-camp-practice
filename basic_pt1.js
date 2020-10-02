8e85348d61bbd755edd77680276169d355e22c9d
DanielG <danielpaskalis.g@gmail.com>
2020-10-02 13:28:36 +0700

    latihan memasukan variable

---
+++ basic_pt1.js
@@ -0,0 +1,12 @@
+/* Understanding Uninitialized Variables */
+var x; x= 9;
+var y; y= 2;
+var z; z= "Kota";
+/* deklarasikan var x y z dengan value 28, 10, dan kota solo dibawah baris ini*/ 
+x= x + 19;
+y= y +8;
+z= z + "Solo";
+
+console.log(x);
+console.log(y);
+console.log(z);
