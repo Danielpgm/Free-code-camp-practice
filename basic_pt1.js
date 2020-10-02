3f6ed136ed674156af545bf540af5fd2c2d4d5a3
DanielG <danielpaskalis.g@gmail.com>
2020-10-02 13:42:22 +0700

    memasukan console log

--- basic_pt1.js
+++ basic_pt1.js
@@ -1,7 +1,7 @@
 /* Understanding Uninitialized Variables */
 var x; x= 9;
 var y; y= 2;
-var z; z= "Kota";
+var z; z= "Kota ";
 /* deklarasikan var x y z dengan value 28, 10, dan kota solo dibawah baris ini*/ 
 x= x + 19;
 y= y +8;
